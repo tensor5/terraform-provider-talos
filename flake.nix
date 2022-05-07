eac10c055465007a20534de627e1ca1a2f682bd2
Nicola Squartini <tensor5@gmail.com>
2022-05-07 19:40:03 +0200

    feat: add flake.nix with default dev shell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Terraform Provider for Talos";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { flake-utils, nixpkgs, self }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.go_1_18
+            pkgs.go-outline
+            pkgs.go-tools
+            pkgs.gopls
+            pkgs.talosctl
+            pkgs.terraform
+          ];
+        };
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
